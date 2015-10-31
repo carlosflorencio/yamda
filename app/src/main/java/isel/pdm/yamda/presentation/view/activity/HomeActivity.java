8b637d9f881331baf55831ea819ed06dc7ff27b1
Carlos Florencio <carlosmflorencio@gmail.com>
2015-10-31 23:56:46 +0000

    Created ToolbarActivity

--- app/src/main/java/isel/pdm/yamda/presentation/view/activity/HomeActivity.java
+++ app/src/main/java/isel/pdm/yamda/presentation/view/activity/HomeActivity.java
@@ -1,26 +1,15 @@
 package isel.pdm.yamda.presentation.view.activity;
 
-import android.app.SearchManager;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SearchView;
-
-import java.util.Locale;
 
 import isel.pdm.yamda.R;
-import isel.pdm.yamda.presentation.view.activity.common.BaseActivity;
+import isel.pdm.yamda.presentation.view.activity.common.ToolbarActivity;
 import isel.pdm.yamda.presentation.view.fragment.ViewPagerAdapter;
 import vendor.SlidingTabLayout;
 
-public class HomeActivity extends BaseActivity {
+public class HomeActivity extends ToolbarActivity {
 
-    Toolbar toolbar;
     ViewPager pager;
     ViewPagerAdapter adapter;
     SlidingTabLayout tabs;
@@ -28,8 +17,10 @@ public class HomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.home_layout);
-        setUpToolbar();
+        this.setUpToolbar();
+
 
         String[] titles = {
                 getResources().getString(R.string.page_theaters),
@@ -58,47 +49,6 @@ public class HomeActivity extends BaseActivity {
 
         // Setting the ViewPager For the SlidingTabsLayout
         tabs.setViewPager(pager);
-
-        Log.v("DEBUG", Locale.getDefault().getLanguage());
-
     }
 
-    private void setUpToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
-        setSupportActionBar(toolbar);
-
-        toolbar.setLogo(R.mipmap.ic_launcher);
-        toolbar.setTitle(R.string.app_name);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        // Set a call to Searchable activity from the search widget
-        SearchManager searchManager =
-                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView =
-                (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {   // New options to menu
-            case R.id.action_about:
-                Intent intent = new Intent(this, AboutActivity.class);
-                startActivity(intent);
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 }
---
+++ app/src/main/java/isel/pdm/yamda/presentation/view/activity/common/ToolbarActivity.java
@@ -0,0 +1,81 @@
+package isel.pdm.yamda.presentation.view.activity.common;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
+
+import isel.pdm.yamda.R;
+import isel.pdm.yamda.presentation.view.activity.AboutActivity;
+
+public abstract class ToolbarActivity extends BaseActivity {
+
+    protected Toolbar toolbar;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected void setUpToolbar() {
+        this.toolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle(R.string.app_name);
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_spinner);
+        progressBar.setVisibility(View.VISIBLE);
+
+        Log.v(TAG, "setup toolbar");
+    }
+
+    /**
+     * Associate the search widget to the toolbar (action bar)
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Set a call to Searchable activity from the search widget
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        return true;
+    }
+
+    /**
+     * Handle action bar options clicks
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {   // New options to menu
+            case R.id.action_about:
+                Intent intent = new Intent(this, AboutActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
