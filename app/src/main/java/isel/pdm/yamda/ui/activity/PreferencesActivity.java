f1f7dc2ceceacbf0313d32cf220e2629b2e64aa1
no0law1 <nuno.m.reis@netcabo.pt>
2016-01-17 17:35:41 +0000

    Preference activity knows when preferences has been changed

--- app/src/main/java/isel/pdm/yamda/ui/activity/PreferencesActivity.java
+++ app/src/main/java/isel/pdm/yamda/ui/activity/PreferencesActivity.java
@@ -2,6 +2,7 @@ package isel.pdm.yamda.ui.activity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 import isel.pdm.yamda.R;
 import isel.pdm.yamda.YamdaApplication;
@@ -20,12 +21,20 @@ public class PreferencesActivity extends ToolbarActivity implements SharedPrefer
 
         this.enableBackButton();
 
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+
         getFragmentManager().beginTransaction()
                             .replace(R.id.content, new PreferencesFragment()).commit();
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        ((YamdaApplication)getApplication()).initPeriodicUpdates();
+        int days = Integer.parseInt(sharedPreferences.getString(key, "7"));
+
+        if(key.equals(getResources().getString(R.string.soon_periodicity))){
+            ((YamdaApplication)getApplication()).refreshSoonAlarm(days);
+        } else if(key.equals(getResources().getString(R.string.soon_periodicity))){
+            ((YamdaApplication)getApplication()).refreshTheatersAlarm(days);
+        }
     }
 }
