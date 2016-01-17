36299953b32955b6e71cf6481cb18184ea8e9c6f
Carlos Florencio <carlosmflorencio@gmail.com>
2016-01-17 19:36:11 +0000

    Added alarm for all thursdays

--- app/src/main/java/isel/pdm/yamda/YamdaApplication.java
+++ app/src/main/java/isel/pdm/yamda/YamdaApplication.java
@@ -12,6 +12,9 @@ import android.preference.PreferenceManager;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import isel.pdm.yamda.data.services.lists.SoonListService;
 import isel.pdm.yamda.data.services.lists.TheatersListService;
 
@@ -47,26 +50,64 @@ public class YamdaApplication extends Application {
     |--------------------------------------------------------------------------
     */
 
+    /**
+     * Register the alarms for updating lists services
+     */
     public void initPeriodicUpdates() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        int soonDays = 7;
         int inTheatersDays = Integer.parseInt(sharedPreferences.getString(getResources().getString(R.string.in_theaters_periodicity), "7"));
 
         setAlarm(new Intent(this, TheatersListService.class), inTheatersDays, PendingIntent.FLAG_NO_CREATE);
-        setAlarm(new Intent(this, SoonListService.class), soonDays, PendingIntent.FLAG_NO_CREATE);
+        setAlarmEveryThursday(new Intent(this, SoonListService.class), PendingIntent.FLAG_NO_CREATE);
     }
 
+    /**
+     * Replace alarm
+     * @param days
+     */
     public void refreshTheatersAlarm(int days){
         Intent intent = new Intent(this, TheatersListService.class);
         setAlarm(intent, days, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
+    /**
+     * Set a repeating intent
+     * @param intent
+     * @param days
+     * @param flag
+     */
     private void setAlarm(Intent intent, int days, int flag) {
         alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
                                          SystemClock
                                                  .elapsedRealtime() + days * AlarmManager.INTERVAL_DAY,
                                          days * AlarmManager.INTERVAL_DAY,
+                                         PendingIntent.getService(this, 0, intent, flag));
+    }
+
+    /**
+     * Repeat intent every thursday
+     * @param intent
+     * @param flag
+     */
+    private void setAlarmEveryThursday(Intent intent, int flag) {
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+
+        Date now = new Date(System.currentTimeMillis());
+        if (cal.getTime().before(now)) {
+            cal.add(Calendar.WEEK_OF_MONTH, 1);
+        }
+
+        long firstTime = cal.getTime().getTime();
+
+        alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                         firstTime,
+                                         7 * AlarmManager.INTERVAL_DAY,
                                          PendingIntent.getService(this, 0, intent, flag));
     }
 
